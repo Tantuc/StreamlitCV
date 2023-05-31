4d2c4e8130a8b80341e07ac5dde20dc09a0598d8
Petr Kushnir <peterkushnir@gmail.com>
2023-05-31 14:50:03 +0200

    Updated code

--- app.py
+++ app.py
@@ -2,15 +2,22 @@ import av
 import cv2
 import streamlit as st
 from streamlit_webrtc import WebRtcMode, webrtc_streamer
+from twilio.rest import Client
 
 def get_ice_servers():
-    return [{"urls": ["stun:stun.l.google.com:19302"]}]
-
+    try:
+        account_sid = st.secrets["twilio_account_sid"]
+        auth_token = st.secrets["twilio_auth_token"]
+        client = Client(account_sid, auth_token)
+        token = client.tokens.create()
+        return token.ice_servers
+    except Exception as e:
+        st.warning(f"Failed to retrieve Twilio ICE servers: {str(e)}")
+        return [{"urls": ["stun:stun.l.google.com:19302"]}]
 
 def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
     image = frame.to_ndarray(format="bgr24")
     return av.VideoFrame.from_ndarray(image, format="bgr24")
-
 
 webrtc_ctx = webrtc_streamer(
     key="object-detection",
