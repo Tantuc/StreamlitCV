43f9f2a6a39f32ed67476d1fbc10095683b22242
Petr Kushnir <peterkushnir@gmail.com>
2023-05-31 14:21:09 +0200

    Updated code

--- app.py
+++ app.py
@@ -1,28 +1,28 @@
+import av
 import cv2
 import streamlit as st
-from streamlit_webrtc import WebRtcMode, webrtc_streamer, VideoTransformerBase
+from streamlit_webrtc import WebRtcMode, webrtc_streamer
 
-FACE_DETECTOR_PATH = "haarcascade_frontalface_default.xml"
+def get_ice_servers():
+    return [{"urls": ["stun:stun.l.google.com:19302"]}]
 
-class FaceDetector(VideoTransformerBase):
-    def __init__(self):
-        self.face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + FACE_DETECTOR_PATH)
 
-    def transform(self, frame):
-        img = frame.to_ndarray(format="bgr24")
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = self.face_cascade.detectMultiScale(gray, 1.1, 4)
+def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
+    image = frame.to_ndarray(format="bgr24")
+    return av.VideoFrame.from_ndarray(image, format="bgr24")
 
-        for (x, y, w, h) in faces:
-            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
-        return img
-
-st.title("Real-time Face Detection")
-st.markdown("This app uses the Streamlit-WebRTC component to access the webcam in real time and perform face detection using OpenCV.")
 
 webrtc_ctx = webrtc_streamer(
-    key="example1",
-    video_transformer_factory=FaceDetector,
-    mode=WebRtcMode.SENDRECV
+    key="object-detection",
+    mode=WebRtcMode.SENDRECV,
+    rtc_configuration={"iceServers": get_ice_servers()},
+    video_frame_callback=video_frame_callback,
+    media_stream_constraints={"video": True, "audio": False},
+    async_processing=True,
+)
+
+st.markdown(
+    "This demo uses a model and code from "
+    "https://github.com/robmarkcole/object-detection-app. "
+    "Many thanks to the project."
 )
